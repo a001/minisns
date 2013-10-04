5b44d982f81c49d0378265df51e852427f562d4e
choibin <kwo2002@gmail.com>
2013-10-04 16:01:02 +0900

    .

--- LeftRightVoice/src/com/trouble/lrv/core/controlleradvice/ExceptionAdvice.java
+++ LeftRightVoice/src/com/trouble/lrv/core/controlleradvice/ExceptionAdvice.java
@@ -1,9 +1,5 @@
 package com.trouble.lrv.core.controlleradvice;
 
-import org.apache.catalina.connector.ClientAbortException;
-import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * @author Choi Bin
